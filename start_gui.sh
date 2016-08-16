b45f2b47196f3bd51f1bf30a3fe1f15047d22ca4
Thomas Brand <tom@trellis.ch>
2016-08-16 15:36:16 +0200

    add modified query tool, allowing ctrl+enter to execute query

--- start_gui.sh
+++ start_gui.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #java -cp archive/mckoidb.jar com.mckoi.tools.JDBCQueryTool -url "jdbc:mckoi:local://./db.conf" -u "admin" -p "admin"
-java -cp archive/mckoidb.jar com.mckoi.tools.JDBCQueryTool -url "jdbc:mckoi://localhost" -u "admin" -p "admin"
+java -cp .:_build:archive/mckoidb.jar util.JDBCQueryToolSimple -url "jdbc:mckoi://localhost" -u "admin" -p "admin"
