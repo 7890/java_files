9a5bb2003c9441fa6e53c49595a1f962bbc692f6
Thomas Brand <tom@trellis.ch>
2016-08-29 23:15:50 +0200

    add functions el, a, ahref

--- src/com/mckoi/database/CustomFunctionFactory.java
+++ src/com/mckoi/database/CustomFunctionFactory.java
@@ -4,13 +4,31 @@ import com.mckoi.database.global.*;
 import java.security.MessageDigest;
 
 /*
-adds two functions to be used in sql queries:
+adds functions to be used in sql queries:
 
 	md5_hash(string)
 	sha1_hash(string)
+	el(name,content,attrs)
+	a(name,value)
 
 i.e.
 	select md5_hash(concat(field1,field2,'foo')) as md5,* from bar;
+
+select
+	el(
+		'a'
+		,displayname
+		,concat(
+			a('href',concat('http://foo.bar/',id))
+			,a('target','_blank')
+		)
+	)
+,*
+from tbl_file
+
+select ahref(concat('http://foo.bar/',id),'_blank',displayname) from tbl_file
+
+/!\\ escaping not considered atm
 
 db.conf must contain
 	function_factories=com.mckoi.database.CustomFunctionFactory
@@ -29,6 +47,9 @@ public class CustomFunctionFactory extends FunctionFactory
 	{
 		addFunction(MD5HashFunction.function_name, MD5HashFunction.class);
 		addFunction(SHA1HashFunction.function_name, SHA1HashFunction.class);
+		addFunction(XMLElementFunction.function_name, XMLElementFunction.class);
+		addFunction(XMLAttributeFunction.function_name, XMLAttributeFunction.class);
+		addFunction(AHREFFunction.function_name, AHREFFunction.class);
 	}
 
 //========================================================================
@@ -69,13 +90,13 @@ public class CustomFunctionFactory extends FunctionFactory
 		public TObject evaluate(GroupResolver group, VariableResolver resolver,
 			QueryContext context)
 		{
-			TObject ob = getParameter(0).evaluate(group, resolver, context);
-			if (ob.isNull())
+			TObject ob0 = getParameter(0).evaluate(group, resolver, context);
+			if (ob0.isNull() || ob0.getObject().toString().equals(""))
 			{
-				return ob;
+				return null;
 			}
-			return new TObject(ob.getTType(),
-				toMD5HashString(ob.getObject().toString())
+			return new TObject(ob0.getTType(),
+				toMD5HashString(ob0.getObject().toString())
 			);
 		}
 
@@ -122,13 +143,13 @@ public class CustomFunctionFactory extends FunctionFactory
 		public TObject evaluate(GroupResolver group, VariableResolver resolver,
 			QueryContext context)
 		{
-			TObject ob = getParameter(0).evaluate(group, resolver, context);
-			if (ob.isNull())
+			TObject ob0 = getParameter(0).evaluate(group, resolver, context);
+			if (ob0.isNull() || ob0.getObject().toString().equals(""))
 			{
-				return ob;
+				return null;
 			}
-			return new TObject(ob.getTType(),
-				toSHA1HashString(ob.getObject().toString())
+			return new TObject(ob0.getTType(),
+				toSHA1HashString(ob0.getObject().toString())
 			);
 		}
 
@@ -138,5 +159,169 @@ public class CustomFunctionFactory extends FunctionFactory
 			return TType.STRING_TYPE;
 		}
 	}//end SHA1HashFunction
+
+//========================================================================
+//========================================================================
+	private static class XMLElementFunction extends AbstractFunction
+	{
+		final static String function_name="el";
+//========================================================================
+		public XMLElementFunction(Expression[] params)
+		{
+			super(function_name, params);
+			if (parameterCount() != 3)
+			{
+				throw new RuntimeException("function '"+function_name+"' must have 3 arguments: element name, content, attrs");
+			}
+		}
+
+//========================================================================
+		public TObject evaluate(GroupResolver group, VariableResolver resolver,
+			QueryContext context)
+		{
+			TObject ob0 = getParameter(0).evaluate(group, resolver, context);
+			TObject ob1 = getParameter(1).evaluate(group, resolver, context);
+			TObject ob2 = getParameter(2).evaluate(group, resolver, context);
+
+			if (ob0.isNull() || ob0.getObject().toString().equals(""))
+			{
+				return null;
+			}
+			String attrs="";
+			if(ob2.isNull() || ob2.getObject().toString().equals(""))
+			{
+			}
+			else
+			{
+				attrs=" "+ob2.getObject().toString();
+			}
+
+			String content="";
+			if(ob1.isNull() || ob1.getObject().toString().equals(""))
+			{
+			}
+			else
+			{
+				content=ob1.getObject().toString();
+			}
+			return new TObject(
+				ob0.getTType(),
+				"<"+ob0.getObject().toString()+attrs+">"
+				+content
+				+"</"+ob0.getObject().toString()+">"
+			);
+		}
+
+//========================================================================
+		public TType returnTType()
+		{
+			return TType.STRING_TYPE;
+		}
+	}//end XMLElementFunction
+
+//========================================================================
+//========================================================================
+	private static class XMLAttributeFunction extends AbstractFunction
+	{
+		final static String function_name="a";
+//========================================================================
+		public XMLAttributeFunction(Expression[] params)
+		{
+			super(function_name, params);
+			if (parameterCount() != 2)
+			{
+				throw new RuntimeException("function '"+function_name+"' must have 2 arguments: attribute name, value");
+			}
+		}
+
+//========================================================================
+		public TObject evaluate(GroupResolver group, VariableResolver resolver,
+			QueryContext context)
+		{
+			TObject ob0 = getParameter(0).evaluate(group, resolver, context);
+			TObject ob1 = getParameter(1).evaluate(group, resolver, context);
+
+			if (ob0.isNull() || ob0.getObject().toString().equals(""))
+			{
+				return null;
+			}
+			String value="";
+			if(ob1.isNull() || ob1.getObject().toString().equals(""))
+			{
+			}
+			else
+			{
+				value=ob1.getObject().toString();
+			}
+			return new TObject(
+				ob0.getTType(),
+				ob0.getObject().toString()+"=\""+value+"\" "
+			);
+		}
+
+//========================================================================
+		public TType returnTType()
+		{
+			return TType.STRING_TYPE;
+		}
+	}//end XMLAttributeFunction
+
+//========================================================================
+//========================================================================
+	private static class AHREFFunction extends AbstractFunction
+	{
+		final static String function_name="ahref";
+//========================================================================
+		public AHREFFunction(Expression[] params)
+		{
+			super(function_name, params);
+			if (parameterCount() != 3)
+			{
+				throw new RuntimeException("function '"+function_name+"' must have 3 arguments: url, target, title");
+			}
+		}
+
+//========================================================================
+		public TObject evaluate(GroupResolver group, VariableResolver resolver,
+			QueryContext context)
+		{
+			TObject ob0 = getParameter(0).evaluate(group, resolver, context);
+			TObject ob1 = getParameter(1).evaluate(group, resolver, context);
+			TObject ob2 = getParameter(2).evaluate(group, resolver, context);
+
+			if (ob0.isNull() || ob0.getObject().toString().equals(""))
+			{
+				return null;
+			}
+			String target="";
+			if(ob1.isNull() || ob1.getObject().toString().equals(""))
+			{
+			}
+			else
+			{
+				target=" target=\""+ob1.getObject().toString()+"\"";
+			}
+			String title="";
+			if(ob2.isNull() || ob1.getObject().toString().equals(""))
+			{
+			}
+			else
+			{
+				title=ob2.getObject().toString();
+			}
+			return new TObject(
+				ob0.getTType(),
+				"<a href=\""+ob0.getObject().toString()+"\""+target+">"
+				+title+"</a>"
+			);
+		}
+
+//========================================================================
+		public TType returnTType()
+		{
+			return TType.STRING_TYPE;
+		}
+	}//end AHREFFunction
+
 }//end class CustomFunctionFactory
 //EOF
