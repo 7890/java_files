0b034eaf25da5481a0394aab21bc4447b10e1752
Thomas Brand <tom@trellis.ch>
2016-08-30 13:53:57 +0200

    respect from, count settings

--- src/util/formatter/PlainRSFormatter.java
+++ src/util/formatter/PlainRSFormatter.java
@@ -13,11 +13,35 @@ public class PlainRSFormatter extends CommonRSFormatter
 {
 	public boolean enclose_xml=true;
 	public String delimiter="";
+
+//=============================================================================
+	public PlainRSFormatter()
+	{
+	}
+
+//=============================================================================
+	public PlainRSFormatter(boolean enclose_xml)
+	{
+		this.enclose_xml=enclose_xml;
+	}
+
 //=============================================================================
 	public void formatRSImpl(ResultSet rs) throws Exception
 	{
 		ResultSetMetaData rsmd = rs.getMetaData();
 		int columnCount = rsmd.getColumnCount();
+		int total_records=totalRecords(rs);
+
+		System.err.println("formatRSImpl: "+total_records+" record(s) in resultset. requested from: "+from_record_index+" count: "+record_count);
+
+		if(from_record_index>=0 && from_record_index<total_records)
+		{
+			rs.absolute(from_record_index);
+		}
+		if(record_count<0 || from_record_index>=total_records) //return empty set
+		{
+			record_count=-1; //all
+		}
 		if(enclose_xml)
 		{
 			writeOut("<result>\n");
@@ -25,15 +49,19 @@ public class PlainRSFormatter extends CommonRSFormatter
 
 		//omit header row
 
+		int rows_so_far=0;
+
 		//print rows, concatenated
-		while (rs.next())
+		while ((record_count==-1 || rows_so_far<record_count) && rs.next())
 		{
 			for(int i=1;i<=columnCount;i++)
 			{
 				writeOut(rs.getString(i)+delimiter);
 			}
 			writeOut("\n");
+			rows_so_far++;
 		}
+
 		if(enclose_xml)
 		{
 			writeOut("</result>\n");
