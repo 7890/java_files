eeefc463841de02053734e425a461f6e20280b18
Thomas Brand <tom@trellis.ch>
2016-10-16 21:25:01 +0000

    use correct option

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #java -cp _build -jar archive/mckoidb.jar -c db.conf
-java -cp .:_build:archive/mckoidb.jar com.mckoi.runtime.McKoiDBMain -c db.conf
+java -cp .:_build:archive/mckoidb.jar com.mckoi.runtime.McKoiDBMain -conf db.conf
